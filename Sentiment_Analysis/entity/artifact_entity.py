09fd4fa0c11ebe4b17856fb77a20fff2aa276d93
Shreyash-jiwane09 <jiwane.shrey09@gmail.com>
2025-06-05 17:45:11 +0530

    Add Data Validation Artifacts

--- Sentiment_Analysis/entity/artifact_entity.py
+++ Sentiment_Analysis/entity/artifact_entity.py
@@ -8,6 +8,18 @@ class DataIngestionArtifacts:
 
 
 @dataclass
+class DataValidationArtifact:
+    schema_validation_status: bool
+    missing_values_status: bool
+    validated_raw_data_path: str
+    validated_imbalance_data_path: str
+    raw_data_file_path: str
+    imbalance_data_file_path: str
+    validation_status: bool
+    message: str
+    schema_file_path: str
+
+@dataclass
 class DataTransformationArtifacts:
     transformed_data_path: str
     tokenizer_path: str = 'tokenizer.pickle'
