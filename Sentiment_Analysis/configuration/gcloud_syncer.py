77580f22d377b0bacad9364d282291a347357036
Shreyash-jiwane09 <jiwane.shrey09@gmail.com>
2025-06-05 17:42:49 +0530

    Update functions specifying Windows machine

--- Sentiment_Analysis/configuration/gcloud_syncer.py
+++ Sentiment_Analysis/configuration/gcloud_syncer.py
@@ -1,11 +1,24 @@
 import os
+import subprocess
+import shlex
+import platform
 
 class GCloudSync:
 
     def sync_folder_to_gcloud(self, bucket_name, filepath, filename):
-        command = f"gsutil cp {filepath}/{filename} gs://{bucket_name}/"
+        full_path = os.path.abspath(os.path.join(filepath, filename))
+
+        # Use double quotes for paths with spaces or Windows-style backslashes
+        if platform.system() == "Windows":
+            full_path = f'"{full_path}"'  # Wrap path in double quotes
+
+        command = f"gsutil cp {full_path} gs://{bucket_name}/"
         os.system(command)
 
     def sync_folder_from_gcloud(self, bucket_name, folder_name, destination):
-        command = f"gsutil cp gs://{bucket_name}/{folder_name} {destination}/{folder_name}"
+        destination_path = os.path.abspath(destination)
+        if platform.system() == "Windows":
+            destination_path = f'"{destination_path}"'
+
+        command = f"gsutil cp gs://{bucket_name}/{folder_name} {destination_path}/{folder_name}"
         os.system(command)
